94e9608eb77d2bf3143578debb6630b82a845dfa
Henrique Gouveia <gouveia@Henriques-MacBook-Pro.local>
2014-06-02 17:38:56 -0300

    HGImageView was added

---
+++ HGComponents/HGImageView/HGImageView.h
@@ -0,0 +1,13 @@
+//
+//  HGImageView.h
+//  HGComponents
+//
+//  Created by Henrique Gouveia on 6/2/14.
+//  Copyright (c) 2014 Henrique Gouveia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGImageView : UIImageView
+
+@end
---
+++ HGComponents/HGImageView/HGImageView.m
@@ -0,0 +1,31 @@
+//
+//  HGImageView.m
+//  HGComponents
+//
+//  Created by Henrique Gouveia on 6/2/14.
+//  Copyright (c) 2014 Henrique Gouveia. All rights reserved.
+//
+
+#import "HGImageView.h"
+
+@implementation HGImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
