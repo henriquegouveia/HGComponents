b532db611180b6c86d4ac8ab4fe9d7b81e80b482
Henrique Gouveia <hgouveia@icloud.com>
2014-11-24 14:37:23 -0200

    HGLabel was added

---
+++ HGComponents/HGLabel/HGLabel.h
@@ -0,0 +1,13 @@
+//
+//  HGLabel.h
+//  HGComponents
+//
+//  Created by Henrique Cesar Gouveia on 11/24/14.
+//  Copyright (c) 2014 Henrique Gouveia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGLabel : UILabel
+
+@end
---
+++ HGComponents/HGLabel/HGLabel.m
@@ -0,0 +1,21 @@
+//
+//  HGLabel.m
+//  HGComponents
+//
+//  Created by Henrique Cesar Gouveia on 11/24/14.
+//  Copyright (c) 2014 Henrique Gouveia. All rights reserved.
+//
+
+#import "HGLabel.h"
+
+@implementation HGLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
